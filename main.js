de54328d1209cd73af38f8d2b33ef96678c6b8da
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-31 17:09:50 -0500

    Removed use of webpack

---
+++ main.js
@@ -0,0 +1,5 @@
+const home = require('./views/home.html');
+const about = require('./views/about.html');
+const login = require('./views/login.html');
+const signup = require('./views/signup.html');
+const maincss = require('./resources/css/main.css');
