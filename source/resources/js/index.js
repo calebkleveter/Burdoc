ea485970c01d10689ce3fbf8389ddcd3dfc602ae
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-31 16:21:00 -0500

    Created base vue for application

--- source/resources/js/index.js
+++ source/resources/js/index.js
@@ -1,1 +1,11 @@
 const Vue = require('vue');
+const BurdocHeader = require('./components/burdoc-header');
+const BurdocForm = require('./components/burdoc-form');
+
+new Vue({
+  el: "#app",
+  components: {
+    burdoc-header: { BurdocHeader },
+    burdoc-form: { BurdocForm }
+  }
+});
