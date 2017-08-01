f43fdfbe905cae452a14e8533343ea7206a3a9ca
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-01 07:52:02 -0500

    Created router.setRequestAndResponse(request, response) method

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
 const route = require('./routeBuilder');
 
 module.exports = {
-  
+  /**
+   * Sets the router's request and response to use in creating routes.
+   *
+   * @param {http.IncomingMessage} request: The request object for creating routes.
+   * @param {http.ServerResponse} response: The response object for creating routes.
+   */
+  setRequestAndResponse: function(request, response) {
+    this.request = request;
+    this.response = response;
+  }
 }
