2d516f82210f61f72936186281b12f67b5ec2cd1
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-04 07:32:21 -0500

    Sync user table in main executable

--- source/main.js
+++ source/main.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const router = require('./router');
 const database = require('./database');
+const user = require('./models/user');
 
 database.connect();
+user.sync();
 
 http.createServer(function(request, response){
   router.registerRoutes(request, response);
