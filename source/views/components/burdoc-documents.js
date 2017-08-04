6d82150723cea907f4d52bb009b608d0101937c8
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-04 17:47:04 -0500

    Moved bootstrap grids to div.docs-loop

--- source/views/components/burdoc-documents.js
+++ source/views/components/burdoc-documents.js
@@ -1,8 +1,8 @@
 Vue.component('burdoc-documents', {
   template: `
   <div id="documents">
-    <div id="docs-loop" v-for="document in documents">
-      <div class="document col-xs-12 col-sm-6 col-md-4 col-lg-3">
+    <div class="docs-loop col-xs-12 col-sm-6 col-md-4 col-lg-3" v-for="document in documents">
+      <div class="document">
         <div class="identifier">
           <p>{{ document.titleCharacter }}</p>
         </div>
@@ -19,7 +19,7 @@ Vue.component('burdoc-documents', {
         {titleCharacter: 'T', title: "Test Doc"},
         {titleCharacter: 'B', title: "Burdoc"},
         {titleCharacter: 'O', title: "On War and Peace"},
-        {titleCharacter: 'U', title: "User Authentication in Vapor 2"},
+        {titleCharacter: 'U', title: "User Authentication in Vapor"},
         {titleCharacter: 'R', title: "The Revenge of the Swift"},
         {titleCharacter: '1', title: "101 Ways to Eat Eggplant"},
         {titleCharacter: 'C', title: "Click Me"},
