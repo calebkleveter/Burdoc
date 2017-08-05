af55f75ba75b168106484004612e4b52ed440509
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-05 07:41:47 -0500

    Commented main executable

--- source/main.js
+++ source/main.js
@@ -3,11 +3,16 @@ const router = require('./router');
 const database = require('./database');
 const user = require('./models/user');
 
+// Connect to the PostgreSQL database used to store users.
 database.connect();
+
+// Sync the user table in the PostgreSQL database.
 user.sync();
 
+// Create and start the server on port 8080.
 http.createServer(function(request, response){
   router.registerRoutes(request, response);
 }).listen(8080);
 
+// Output that the server has started.
 console.log("Running server on port 8080");
