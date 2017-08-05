e832abce61408b9b70f0fc425ac524efffc89642
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-05 07:49:14 -0500

    Documented the database module

--- source/database.js
+++ source/database.js
@@ -1,10 +1,16 @@
 const Sequelize = require('sequelize');
 
+/**
+ * A instance of Sequelize which is connected to the PostgreSQL database.
+ */
 const sequelize = new Sequelize('burdoc', 'calebkleveter', '', {
   host: 'localhost',
   dialect: 'postgres'
 });
 
+/**
+ * Connects the `sequelize` constent to the database by checking the authentication.
+ */
 function connect() {
   sequelize.authenticate()
            .then(function(){
