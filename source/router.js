ebc1ae4fd8887d2c8d44cbcce434a46ff7cc8c36
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-05 07:46:45 -0500

    Documented route functions in the router module

--- source/router.js
+++ source/router.js
@@ -22,30 +22,45 @@ module.exports = {
     response.end();
   },
 
+  /**
+   * The route for the root path, which sends the home.html view to the reponse.
+   */
   home: function() {
     route.get('/', function(){
       return view.get('home');
     });
   },
 
+  /**
+   * The route for the /about path, which sends the about.html view to the reponse.
+   */
   about: function() {
     route.get('/about', function(){
       return view.get('about');
     });
   },
 
+  /**
+   * The route for the /login path, which sends the login.html view to the reponse.
+   */
   login: function() {
     route.get('/login', function(){
       return view.get('login');
     });
   },
 
+  /**
+   * The route for the /signup path, which sends the signup.html view to the reponse.
+   */
   signup: function() {
     route.get('/signup', function(){
       return view.get('signup');
     });
   },
 
+  /**
+   * The route for the /dashboard path, which sends the dashboard.html view to the reponse.
+   */
   dashboard: function() {
     route.get('/dashboard', function(){
       return view.get('dashboard');
