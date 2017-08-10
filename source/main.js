77b990d3f0be65824783ceb5e732d2b26d464254
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-10 21:07:21 +0000

    Configured HTTPS server

--- source/main.js
+++ source/main.js
@@ -1,8 +1,14 @@
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const sockets = require('./sockets');
 const router = require('./router');
 const database = require('./database');
 const user = require('./models/user');
+
+const serverOptions = {
+    key: fs.readFileSync(`${__dirname}/../secrets/key.pem`),
+    cert: fs.readFileSync(`${__dirname}/../secrets/cert.pem`)
+};
 
 // Connect to the PostgreSQL database used to store users.
 database.connect();
@@ -11,9 +17,9 @@ database.connect();
 user.sync();
 
 // Create and start the server on port 8080.
-const server = http.createServer(function (request, response) {
+const server = https.createServer(serverOptions, function (request, response) {
   router.registerRoutes(request, response);
-}).listen(8080);
+}).listen(2368);
 
 sockets.configure(server);
 
