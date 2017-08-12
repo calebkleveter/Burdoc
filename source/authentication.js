92e2339eabc1b321837581a1b241e66795248a13
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-12 12:03:39 +0000

    Created authentication.setServer(server) function

--- source/authentication.js
+++ source/authentication.js
@@ -1,7 +1,14 @@
 const crypto = require('crypto');
 
 var authentication = {
-  
+    /**
+   * Sets the server the authentication middleware will connect to.
+   * 
+   * @param {http.ClientRequest} server: The server the middleware will connect to.
+   */
+  setServer: function (server)  {
+    this.server = server;
+  }
 };
 
 module.exports = authentication
