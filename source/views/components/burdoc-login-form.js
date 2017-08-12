3e0f4f6d61bc48cadded1bec4677dc80a0c64379
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-12 16:59:06 +0000

    Added login indicator, redirection, and error handling to burdoc-login-form vue component

--- source/views/components/burdoc-login-form.js
+++ source/views/components/burdoc-login-form.js
@@ -2,31 +2,51 @@ Vue.component('burdoc-login-form', {
   props: ['submittext'],
   template: `
   <div class="form">
-    <form>
+    <div v-if='isSubmitting' class="loading-indicator">
+     <i class="fa fa-cog fa-5x fa-spin" aria-hidden="true"></i>
+    </div>
+    <form v-else>
       <div class="form-group">
         <label for="username">Username:</label>
-        <input type="text" name="username" v-model='username' class="form-control" id="username" placeholder="Username">
+        <input type="text" name="username" v-model='username' class="form-control" id="username" @keydown="reset" placeholder="Username">
       </div>
       <div class="form-group">
         <label for="password">Password:</label>
-        <input type="password" name="password" v-model='password' class="form-control" id="password" placeholder="Password">
+        <input type="password" name="password" v-model='password' class="form-control" id="password" @keydown="reset" placeholder="Password">
       </div>
-      <button type="button" class="btn btn-default" id="signup-btn" @click="login">{{ submittext }}</button>
+      <div v-if='error' class="error-message">
+        <p>{{ error }}</p>
+      </div>
+      <button type="button" class="btn btn-default" id="signup-btn" :disabled="error != ''" @click="login">{{ submittext }}</button>
     </form>
   </div>
   `,
   data: function () {
     return {
+      isSubmitting: false,
       username: '',
-      password: ''
+      password: '',
+      error: ''
     };
   },
   methods: {
     login: function () {
+      this.isSubmitting = true;
       socket.emit('login', {
         username: this.username,
         password: this.password
       });
+      socket.on('loginSuccess', function () {
+        window.location.href = '/dashboard';
+      });
+      socket.on('loginError', (error) => {
+        this.isSubmitting = false;
+        this.error = error;
+      });
+    },
+    
+    reset: function () {
+      this.error = '';
     }
   }
 });
