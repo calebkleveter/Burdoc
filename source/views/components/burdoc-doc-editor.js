4ce547914e98a730998d5e13bb5c65ae5fbf8df2
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-15 15:30:31 +0000

    Added text rendering to burodc-doc-editor view component

--- source/views/components/burdoc-doc-editor.js
+++ source/views/components/burdoc-doc-editor.js
@@ -2,16 +2,22 @@ Vue.component('burdoc-doc-editor', {
   template: `
   <div id="editor" class="col-xs-12 col-sm-12">
     <div id="edit-tab" class="col-md-6">
-      <textarea name="document-editor" id="document-editor" v-model="documentText"></textarea>
+      <textarea name="document-editor" id="document-editor" v-model="documentText" @keyup="render"></textarea>
     </div>
     <div id="preview-tab" class="col-md-6" v-html="html"></div>
     </div>
   </div>
   `,
-  dataq: function () {
+  data: function () {
     return {
       documentText: '',
       html: ''
     };
+  },
+  methods: {
+    render: function () {
+      var converter = new showdown.Converter();
+      this.html = converter.makeHtml(this.documentText);
+    }
   }
 });
