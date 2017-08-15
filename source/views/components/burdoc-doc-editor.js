cb6ac89e0915d5d49582bffaf8e26143b5641bc0
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-15 15:20:14 +0000

    Added data binding to burdc-doc-editor vuew component

--- source/views/components/burdoc-doc-editor.js
+++ source/views/components/burdoc-doc-editor.js
@@ -2,10 +2,16 @@ Vue.component('burdoc-doc-editor', {
   template: `
   <div id="editor" class="col-xs-12 col-sm-12">
     <div id="edit-tab" class="col-md-6">
-      <textarea name="document-editor" id="document-editor"></textarea>
+      <textarea name="document-editor" id="document-editor" v-model="documentText"></textarea>
     </div>
-    <div id="preview-tab" class="col-md-6"></div>
+    <div id="preview-tab" class="col-md-6" v-html="html"></div>
     </div>
   </div>
-  `
+  `,
+  dataq: function () {
+    return {
+      documentText: '',
+      html: ''
+    };
+  }
 });
