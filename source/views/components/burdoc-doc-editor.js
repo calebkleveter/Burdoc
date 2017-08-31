b6f14208c47ef0ba762e9ff0a082bff4af87ced7
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-08-31 07:32:14 -0500

    Created burdoc-doc-editor.download(text, filename, texttype) function to download the doument

--- source/views/components/burdoc-doc-editor.js
+++ source/views/components/burdoc-doc-editor.js
@@ -56,6 +56,26 @@ Vue.component('burdoc-doc-editor', {
       socket.on('saveFailed', function (error) {
         bootbox.alert(error);
       });
+    },
+
+    download: function (text, filename, texttype) {
+      var textFileAsBlob = new Blob([text], {type: `text/${texttype}`});
+      var downloadLink = document.createElement('a');
+      downloadLink.download = filename;
+      downloadLink.innerHTML = 'Download File';
+      if (window.webkitURL != null) {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+      } else {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = 'none';
+        document.body.appendChild(downloadLink);
+      }
+      downloadLink.click();
     }
   },
   created: function () {
