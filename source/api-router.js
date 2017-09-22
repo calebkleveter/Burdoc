3009d5bbf7b450e74678627f37d08e8a72a0476e
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-09-22 09:34:13 -0500

    Created api-router module

---
+++ source/api-router.js
@@ -0,0 +1,12 @@
+const route = require('./routeBuilder');
+const authentication = require('./authentication');
+const user = require('./models/user');
+const document = require('./models/document');
+
+function registerRoutes () {
+
+}
+
+module.exports = {
+  registerRoutes: registerRoutes
+};
