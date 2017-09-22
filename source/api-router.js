746a24f8b4fe4ae948757ccf53093293df40fdda
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-09-22 09:36:33 -0500

    Moved /user-documents route to api-router module

--- source/api-router.js
+++ source/api-router.js
@@ -4,7 +4,35 @@ const user = require('./models/user');
 const document = require('./models/document');
 
 function registerRoutes () {
+  this.userDocuments();
+}
 
+/**
+ * A route for fetching the documents for a user's dashboard.
+ */
+function userDocuments () {
+  route.protected(route.method.post, '/user-documents', function (args) {
+    document.fetchAllForUserID(args.user.id).then(function (documents) {
+      var data = [];
+      documents.forEach(function (doc) {
+        var title = '';
+        if (doc.dataValues.name.length > 24) {
+          title = `${doc.dataValues.name.substring(0, 21)}...`;
+        } else {
+          title = doc.dataValues.name;
+        }
+        data.push({
+          title: title,
+          titleCharacter: doc.dataValues.name[0],
+          url: `document/${args.user.name}/${doc.dataValues.url}`,
+          id: doc.id
+        });
+      });
+      args.finish(JSON.stringify(data));
+    }).catch(function (error) {
+      args.finish(`${{error: error.message}}`);
+    });
+  });
 }
 
 module.exports = {
--- source/web-router.js
+++ source/web-router.js
@@ -133,35 +133,5 @@ module.exports = {
         args.finish();
       }
     }, '/dashboard');
-  },
-
-  // MARK: - Page Support Routes
-
-  /**
-   * A route for fetching the documents for a user's dashboard.
-   */
-  userDocuments: function () {
-    route.protected(route.method.post, '/user-documents', function (args) {
-      document.fetchAllForUserID(args.user.id).then(function (documents) {
-        var data = [];
-        documents.forEach(function (doc) {
-          var title = '';
-          if (doc.dataValues.name.length > 24) {
-            title = `${doc.dataValues.name.substring(0, 21)}...`;
-          } else {
-            title = doc.dataValues.name;
-          }
-          data.push({
-            title: title,
-            titleCharacter: doc.dataValues.name[0],
-            url: `document/${args.user.name}/${doc.dataValues.url}`,
-            id: doc.id
-          });
-        });
-        args.finish(JSON.stringify(data));
-      }).catch(function (error) {
-        args.finish(`${{error: error.message}}`);
-      });
-    });
   }
 };
