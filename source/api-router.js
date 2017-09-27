1b96dbadcc1f2dde2264bbb6bb95cc56e454d53b
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-09-27 08:19:40 -0500

    Created API routes for createing a tag, a document, fetching all the tags for a document, all tags for all documents, and for removing a tag.

--- source/api-router.js
+++ source/api-router.js
@@ -6,8 +6,25 @@ const documentTag = require('./models/document-tag');
 const tag = require('./models/tags');
 
 function registerRoutes () {
+  createDocument();
   userDocuments();
   documentTags();
+  allDocumentTags();
+  removeDocumentTag();
+  createTag();
+}
+
+function createDocument () {
+  route.protected(route.method.post, '/create-document', function (args) {
+    document.create(args.user.id, args.body.name, '').then(function (documentModel) {
+      var doc = {
+        url: `document/${args.user.name}/${documentModel.dataValues.url}`
+      };
+      args.finish(JSON.stringify(doc));
+    }).catch(function (error) {
+      args.finish(JSON.stringify({error: error.message}));
+    });
+  });
 }
 
 /**
@@ -33,29 +50,77 @@ function userDocuments () {
       });
       args.finish(JSON.stringify(data));
     }).catch(function (error) {
-      args.finish(`${{error: error.message}}`);
+      args.finish(JSON.stringify({error: error.message}));
+    });
+  });
+}
+
+function allDocumentTags () {
+  route.protected(route.method.post, '/all-tags', function (args) {
+    document.fetchAllForUserID(args.user.id).then(function (docs) {
+      var tagFetchers = docs.map(function (doc) {
+        return document.fetchTagsForID(doc.id);
+      });
+
+      return Promise.all(tagFetchers);
+    }).then(function (models) {
+      var documentTags = {};
+      models.forEach(function (model) {
+        var tags = model.tags.map(function (tag) {
+          return {
+            id: tag.id,
+            name: tag.name
+          };
+        });
+        documentTags[model.documentID] = tags;
+      });
+      args.finish(JSON.stringify(documentTags));
+    }).catch(function (error) {
+      args.finish(JSON.stringify({error: error.message}));
     });
   });
 }
 
 function documentTags () {
-  route.protected(route.method.get, '/document-tags', function (args) {
-    var documentsComplete = 0;
-    var tags = {};
-    document.fetchAllForUserID(args.user.id).then(function (documents) {
-      documents.forEach(function (doc) {
-        tags[doc.id] = [];
-        documentTag.fetchAllByDocumentID(doc.id).then(function (pivots) {
-          pivots.forEach(function (pivot) {
-            tag.fetchByID(pivot.tagID).then(function (documentTag) {
-              tags[doc.id].push(documentTag);
-              if (documentsComplete > documents.length) {
-                args.finish(tags);
-              }
-            });
-          });
-        });
+  route.protected(route.method.post, '/document-tags', function (args) {
+    document.findByNameAndUserID(args.body.name, args.user.id).then(function (doc) {
+      return document.fetchTagsForID(doc.id);
+    }).then(function (model) {
+      var tags = model.tags.map(function (model) {
+        return {
+          id: model.id,
+          name: model.name
+        };
       });
+
+      args.finish(JSON.stringify(tags));
+    }).catch(function (error) {
+      args.finish(JSON.stringify({error: error.message}));
+    });
+  });
+}
+
+function removeDocumentTag () {
+  route.protected(route.method.post, '/remove-document-tag', function (args) {
+    documentTag.deleteWithDocumentAndTagID(args.body.documentID, args.body.tagID).then(function () {
+      args.finish();
+    }).catch(function (error) {
+      args.finish(JSON.stringify({error: error.message}));
+    });
+  });
+}
+
+function createTag () {
+  route.protected(route.method.post, '/create-tag', function (args) {
+    var newTag;
+    tag.create(args.body.tagName).then(function (model) {
+      newTag = model.dataValues;
+      return documentTag.create(args.body.documentID, model.id);
+    }).then(function () {
+      args.finish(JSON.stringify(newTag));
+    }).catch(function (error) {
+      console.error(error);
+      args.finish(JSON.stringify({error: error.message}));
     });
   });
 }
