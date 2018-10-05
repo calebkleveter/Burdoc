1770283a31f8d7b9fadc04822831fe2e1a7bc3c5
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-05 13:05:41 -0500

    Changed burdoc-signup-form and burdoc-header format from .js file to .vue file

---
+++ source/views/components/burdoc-header.vue
@@ -0,0 +1,51 @@
+<template>
+    <header>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid" id="navigation">
+        <div class="navbar-header col-sm-12 col-md-12 col-lg-12">
+          <div class="logo-link col-xs-12 col-sm-12 col-md-2 col-lg-2">
+            <a class="" href="/">
+              <div class="logo">
+                <img src="/images/burdoc.svg" alt="Alt">
+                <h1>B<span class="small-title">URDOC</span></h1>
+              </div>
+            </a>
+          </div>
+          <div class="col-xs-0 col-sm-0 col-md-5 col-lg-6"></div>
+          <div class="navigation col-xs-12 col-sm-12 col-md-5 col-lg-4">
+            <ul class="nav-links nav navbar-nav nav-bar" v-for="link in links" :key="link.id">
+              <li><a :href="link.url">{{ link.name }}</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    data: function () {
+    return {
+      links: [
+        {url: '/signup', name: 'Sign Up'}, 
+        {url: '/login', name: 'Login'}, 
+        {url: '/about', name: 'About'}, 
+        {url: '/', name: 'Home'}
+      ]
+    };
+  },
+  
+  created: function () {
+    socket.emit('checkForAuthorization')
+    socket.on('authorized', () => {
+      this.links = [
+        {url: '/dashboard', name: 'Dashboard'},
+        {url: '/logout', name: 'Logout'},
+        {url: '/about', name: 'About'}, 
+        {url: '/', name: 'Home'}
+      ]
+    });
+  }
+}
+</script>
---
+++ source/views/components/burdoc-signup-form.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="form">
+    <div v-if='isSubmitting' class="loading-indicator">
+     <i class="fa fa-cog fa-5x fa-spin" aria-hidden="true"></i>
+    </div>
+    <form v-else>
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" name="username" v-model='username' class="form-control" id="username" @keydown="reset" placeholder="Username">
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" name="email" v-model='email' class="form-control" id="email" @keydown="reset" placeholder="Email">
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" name="password" v-model='password' class="form-control" id="password" @keydown="reset"  placeholder="Password">
+      </div>
+      <div v-if='error' class="error-message">
+        <p>{{ error }}</p>
+      </div>
+      <button type="button" class="btn btn-default" id="signup-btn" :disabled="error != ''" @click="signup">{{ submittext }}</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+    return {
+      isSubmitting: false,
+      username: '',
+      email: '',
+      password: '',
+      error: ''
+    };
+  },
+  methods: {
+    signup: function () {
+      this.isSubmitting = true;
+      
+      this.$http.post('/signup', {
+        username: this.username,
+        email: this.email,
+        password: this.password
+      }).then((response) => {
+        if (response.headers.has('Auth-Error')) {
+          this.isSubmitting = false;
+          this.error = response.headers.get('Auth-Error');
+        } else {
+          window.location.href = '/dashboard';
+        }
+      }).catch((error) => {
+        this.error = error.message;
+      });
+    },
+
+    reset: function () {
+      this.error = '';
+    }
+  }
+}
+</script>
+
