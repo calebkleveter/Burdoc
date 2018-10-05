6d2e5c50078fcd20b8ef12cba5b66ccf680c686e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-05 15:17:12 -0500

    Added burdoc-login-form component to main Vue components

--- source/resources/js/index.js
+++ source/resources/js/index.js
@@ -2,8 +2,9 @@ import Vue from './vue.js';
 import BurdocHeader from '../../views/components/burdoc-header.vue';
 import BurdocRenameDocModel from '../../views/components/burdoc-rename-doc-model.vue';
 import BurdocNewDocModel from '../../views/components/burdoc-new-doc-model.vue';
+import BurdocLoginForm from '../../views/components/burdoc-login-form.vue'
 
 new Vue({
   el: "#app",
-  components: { BurdocHeader, BurdocRenameDocModel, BurdocNewDocModel }
+  components: { BurdocHeader, BurdocRenameDocModel, BurdocNewDocModel, BurdocLoginForm }
 });
