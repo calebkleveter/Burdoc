920bbdd0a10e24a2c3b6486f7fec6c2f6e14a4f2
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-05 06:37:16 -0500

    Relocate resource files to /dist directory on build

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const DashboardPlugin = require('webpack-dashboard/plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Merge = require('webpack-merge-and-include-globally');
 
 module.exports = {
   target: 'node',
@@ -39,7 +40,36 @@ module.exports = {
     // new DashboardPlugin(),
     new CopyWebpackPlugin([
       { from: 'source/views', to: 'views' },
-      { from: 'source/resources', to: 'views' }
-    ])
+      { from: 'source/resources/images', to: 'images' },
+      { from: 'source/resources/fonts', to: 'fonts' }
+    ]),
+    new Merge({
+      files: {
+        'css/main.css': [
+          'source/resources/css/bootstrap.min.css',
+          'source/resources/css/font-awesome.min.css',
+          'source/resources/css/main.css'
+        ],
+        'js/index.js': [
+          'source/resources/js/jquery-3.2.1.min.js',
+          'source/resources/js/bootstrap.min.js',
+          'source/resources/js/vue.js',
+          'source/resources/js/showdown.min.js',
+          'source/resources/js/bootbox.min.js',
+          'source/resources/js/jsPDF.min.js',
+          'source/resources/js/html2canvas.js',
+          'source/resources/js/html2pdf.js',
+          'source/resources/js/vue-resource.js',
+          'source/resources/js/burdoc-header.js',
+          'source/resources/js/burdoc-signup-form.js',
+          'source/resources/js/burdoc-login-form.js',
+          'source/resources/js/burdoc-new-doc-model.js',
+          'source/resources/js/burdoc-rename-doc-model.js',
+          'source/resources/js/burdoc-documents.js',
+          'source/resources/js/burdoc-doc-editor.js',
+          'source/resources/js/index.js'
+        ]
+      }
+    })
   ]
 };
