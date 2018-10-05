8ff949f7c9228b1bcda9cb0b9a61c912abfd4a28
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-05 15:44:15 -0500

    Added missing burdoc-signup-form component to main Vue components

--- source/resources/js/index.js
+++ source/resources/js/index.js
@@ -2,7 +2,8 @@ import Vue from './vue.js';
 import BurdocHeader from '../../views/components/burdoc-header.vue';
 import BurdocRenameDocModel from '../../views/components/burdoc-rename-doc-model.vue';
 import BurdocNewDocModel from '../../views/components/burdoc-new-doc-model.vue';
-import BurdocLoginForm from '../../views/components/burdoc-login-form.vue'
+import BurdocLoginForm from '../../views/components/burdoc-login-form.vue';
+import BurdocSignupForm from '../../views/components/burdoc-signup-form.vue';
 import BurdocDocuments from '../../views/components/burdoc-documents.vue';
 import BurdocDocEditor from '../../views/components/burdoc-doc-editor.vue';
 
@@ -13,6 +14,7 @@ new Vue({
     BurdocRenameDocModel,
     BurdocNewDocModel,
     BurdocLoginForm, 
+    BurdocSignupForm,
     BurdocDocuments, 
     BurdocDocEditor 
   }
