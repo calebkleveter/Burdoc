a6ab69d61054b6b8b74d1b06dce78e630914b3a4
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-05 15:11:55 -0500

    Added burdoc-rename-doc-model component to main Vue components

--- source/resources/js/index.js
+++ source/resources/js/index.js
@@ -1,7 +1,8 @@
 import Vue from './vue.js';
 import BurdocHeader from '../../views/components/burdoc-header.vue';
+import BurdocRenameDocModel from '../../views/components/burdoc-rename-doc-model.vue';
 
 new Vue({
   el: "#app",
-  components: { BurdocHeader }
+  components: { BurdocHeader, BurdocRenameDocModel }
 });
